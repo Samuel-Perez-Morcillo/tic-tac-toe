9c810682a5d5d9ef58de2b43c1f761ef4952b470
Samuel Pérez <samuelperez82pm@gmail.com>
2024-06-12 11:52:24 +0200

    Square and Turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+
+  const selectPlayer = `square ${isSelected ? "is-selected" : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
+
+  return (
+    <div onClick={handleClick} className={selectPlayer} >
+      {children}
+    </div >
+  )
+}
 
 function App() {
+  const Turns = {
+    X: "x",
+    O: "o"
+  }
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(Turns.X)
+
+  const updateBoard = () => {
+    const newTurn = turn === Turns.X ? Turns.O : Turns.X
+    setTurn(newTurn)
+  }
 
   return (
     <>
-      <h1>HOLA</h1>
+      <main className="board">
+        <section className="game">
+          {
+            board.map((eachBoard, index) => {
+              return (
+                <Square
+                  key={index}
+                  index={index}
+                  updateBoard={updateBoard}
+                >
+                  {board[index]}
+                </Square>
+              )
+            })
+          }
+        </section>
+
+        <div className="turn">
+          <Square isSelected={turn === Turns.X}>{Turns.X}</Square>
+          <Square isSelected={turn === Turns.O}>{Turns.O}</Square>
+
+        </div>
+      </main>
+
+
     </>
   )
 }
